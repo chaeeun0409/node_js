7f3e9ba3a4510b3c2d14d1d14fbd209a36a9d8f2
chaeeun0409 <yura040909kim@naver.com>
2022-07-29 11:52:15 +0900

    0729

--- session01.js
+++ session01.js
@@ -7,7 +7,8 @@ var cookieParser = require('cookie-parser')
 var router = express.Router();
 
 // 세션 추가
-var expressSession = require('express-session')
+var expressSession = require('express-session');
+const { fileLoader } = require('ejs');
 
 // 익스프레스 시작
 var app = express();
@@ -93,6 +94,55 @@ router.route('/process/setUserCookie').get(function(req, res){
 res.redirect('/process/showCookie');
 
 });*/
+
+
+var storage = multer.diskStorage({
+    destination : function(req,file,callback){
+        callback(null,'upload')
+    },
+    filename : function(req,file,callback){
+        callback(null, file.originalname + Date.now())
+    }
+});
+var upload = multer({
+    storage: storage,
+    limits: {
+        files: 10,
+        fileSize : 1024 * 1024 * 1024
+    }
+});
+//파일 업로드 라우팅 함수 - 로그인 후 세션 저장함
+router.route('/process/photo').post(upload.array('photo',1),function(req,res){
+    console.log('/process/photo 호출됨');
+    var files= req.files;
+
+    console.log('==================== 업로드 된 파일');
+    if(files.length > 0){
+        console.dir(files[0]);
+    }
+    else{
+        console.log('파일이 없습니다.');
+    }
+
+    var originalname = '';
+    var filename = '';
+    var mimetype = '';
+    var size = 0;
+    if(Array.isArray(files)){
+        for(var i = 0; i<files.length; i++){
+            originalname=files[i].originalname;
+            filename=files[i].filename;
+            mimetype=files[i].mimetype;
+            size=files[i].size;
+        }
+    }
+    res.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+    res.write('<h1>파일 업로드 성공</h1>')
+    res.write('<p>원본파일 : ' + originalname + '</p>')
+    res.write('<p>저장파일 : ' + filename + '</p>')
+    res.end();
+
+})
 
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('익스프레스 웹서버 실행 : ' + app.get('port'));
